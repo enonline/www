cabdafd9a7cd2b05a9e58d21a9f5d74725f6c50b
mohsenkhanpour <mohsenkhanpour@gmail.com>
2023-02-21 14:03:33 +0330

    Deploy from Azure DevOps

---
+++ storybook/component---src-pages-samples-static-query-tsx-bb5e864509ffce81426d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkghost_casper=self.webpackChunkghost_casper||[]).push([[701],{7979:function(e,t,n){n.r(t);var l=n(7294),a=n(4160);t.default=()=>{const e=(0,a.useStaticQuery)(s);return l.createElement("div",null,e.allMdx.nodes.map((e=>l.createElement("span",{key:e.id},l.createElement("div",null,e.id)," ",l.createElement("br",null)))))};const s="162947461"}}]);
+//# sourceMappingURL=component---src-pages-samples-static-query-tsx-bb5e864509ffce81426d.js.map
